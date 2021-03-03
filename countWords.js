4d93d340c807d0470a0f610ae898286ad819c03f
mukesh-theavatar <mukesh.ranjan593@gmail.com>
2021-03-03 21:32:09 +0530

    1.a - Solution to CountWords Problem

--- countWords.js
+++ countWords.js
@@ -1,10 +1,7 @@
 
 const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Javascript if you do not love something which can give life to your appication what else can you love.';
-function countWords(paragraph){
-paragraph = paragraph.replace(/(^\s*)|(\s*$)/gi, "");
-paragraph = paragraph.replace(/[ ]{2,}/gi, " ");
-paragraph = paragraph.replace(/\n /, "\n");
-return paragraph.split(' ').length;
+function countWords(paragraph, word1, word2){
+
 }
 //document.write(countWords());
 console.log(countWords(paragraph, 'love', 'you'));
